4d460cde6bdb212aa29efa5c2b76f0c85526dd84
Lascax <gunjack88@hotmail.it>
2011-09-26 17:36:31 +0200

    Prova Nickname

--- src/game/Bag.cpp
+++ src/game/Bag.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "Bag.h"
+#include "Bag.h" 
 #include "ObjectMgr.h"
 #include "Database/DatabaseEnv.h"
 #include "Log.h"
